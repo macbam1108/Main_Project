56740ec494cbb205949f8a37a16aa8fbe9148717
Brandon MacPherson <macbam1108@gmail.com>
2019-10-30 13:27:17 -0600

    Initial commit

---
+++ app/src/main/java/com/macpherson/brandon/mainproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.macpherson.brandon.mainproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
